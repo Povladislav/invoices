d5179ab5015292e9847bbfd2997ab3ec09352e48
Povladislav <vladpomalejko@gmail.com>
2023-07-19 13:32:24 +0300

    little fixes

--- src/repository.py
+++ src/repository.py
@@ -9,15 +9,10 @@ class InvoiceRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    async def get_invoices(self, skip: int, limit: int):
-        # Prepare the query to fetch invoices (without executing it)
+    async def get_invoices(self):
         async with self.session.begin():
             invoices_query = select(Invoice.id, Invoice.title, Invoice.discount)
-            # Execute the query asynchronously
             invoices_results = await self.session.execute(invoices_query)
-            # Get all fetched invoices
             invoices = invoices_results.all()
 
-        # Convert the list of tuples to a list of InvoiceOutSchema objects
-        return [InvoiceOutSchema(id=invoice.id, title=invoice.title, discount=invoice.discount) for invoice in invoices]
-
+        return [Invoice(id=invoice.id, title=invoice.title, discount=invoice.discount) async for invoice in invoices]
