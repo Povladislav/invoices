ebbd301911b4fac83dae3028be2054d60f9a0b20
Povladislav <vladpomalejko@gmail.com>
2023-07-19 12:11:59 +0300

    Base changed

--- src/models/invoice.py
+++ src/models/invoice.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 
-from sqlalchemy import DECIMAL
+from sqlalchemy import DECIMAL, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.database import Base
@@ -9,6 +9,10 @@ from models.base import BaseModel
 
 class Invoice(BaseModel):
     __tablename__ = "invoices"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(ForeignKey("base_model.id"), primary_key=True)
     title: Mapped[str] = mapped_column(nullable=False)
     discount: Mapped[Decimal] = mapped_column(DECIMAL(precision=5, scale=2))
+    __mapper_args__ = {
+        "polymorphic_identity": "invoices",
+        "inherit_condition": id == BaseModel.id
+    }
--- src/models/invoiceline.py
+++ src/models/invoiceline.py
@@ -1,14 +1,18 @@
 from decimal import Decimal
 
 from sqlalchemy.orm import Mapped, mapped_column
-
+from sqlalchemy import ForeignKey, DECIMAL
 from database.database import Base
 from models.base import BaseModel
 
 
 class InvoiceLine(BaseModel):
     __tablename__ = "lines"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(ForeignKey("base_model.id"), primary_key=True)
     title: Mapped[str] = mapped_column(nullable=False)
     quantity: Mapped[int] = mapped_column(nullable=False)
-    price_per_one = Mapped[Decimal] = mapped_column()
+    price_per_one: Mapped[Decimal] = mapped_column()
+    __mapper_args__ = {
+        "polymorphic_identity": "lines",
+        "inherit_condition": id == BaseModel.id
+    }
